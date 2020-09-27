5607e9096ba5c5835ae22e3856e1ee0be7ceb3d3
Gustavo Diaz <gustavo4581@gmail.com>
2020-09-26 20:18:31 -0400

    faltante

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,11 +2,71 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Booking extends Model
 {
-    protected $fillable = ['title', 'description', 'from', 'to', 'popularity_rating'];
+    protected $fillable = ['activity_id', 'n_persons', 'book_price', 'book_date', 'activity_date'];
+    /**
+     * @var Activity
+     */
+    private $activity;
+
+    public function getAll(): Collection
+    {
+        return self::all();
+    }
+
+    // Setters and Getters
+    public function setActivityId(int $value): void
+    {
+        $this->attributes['activity_id'] = $value;
+    }
+    public function getActivityId(): int
+    {
+        return $this->attributes['activity_id'];
+    }
+    public function setNPersons(int $value): void
+    {
+        $this->attributes['n_persons'] = $value;
+    }
+    public function getNPersons(): int
+    {
+        return $this->attributes['n_persons'];
+    }
+    public function setBookPrice(float $value): void
+    {
+        $this->attributes['book_price'] = $value;
+    }
+    public function getBookPrice(): float
+    {
+        return $this->attributes['book_price'];
+    }
+    public function setBookDate(\DateTime $value): void
+    {
+        $this->attributes['book_date'] = $value;
+    }
+    public function getBookDate(): \DateTime
+    {
+        return $this->attributes['book_date'];
+    }
+    public function setActivityDate(\DateTime $value): void
+    {
+        $this->attributes['activity_date'] = $value;
+    }
+    public function getActivityDate(): \DateTime
+    {
+        return $this->attributes['activity_date'];
+    }
+
+
+    // Relationships
+    public function Activity(): HasOne
+    {
+        return $this->hasOne(Activity::class);
+    }
+
 
 }
